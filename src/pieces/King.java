b390c5f03488dc3c42f805e4fd92302ff63e014d
vladzero1 <37445555+vladzero1@users.noreply.github.com>
2020-05-10 22:18:15 +0700

    King Movement (Complete)
    - King move
    - King Attack

--- src/pieces/King.java
+++ src/pieces/King.java
@@ -1,8 +1,11 @@
 package pieces;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import gui.ChessWindow;
 import javafx.util.Pair;
+import mainGame.Grid;
 import mainGame.PiecesType;
 import mainGame.Player;
 
@@ -14,20 +17,158 @@ public class King extends Piece{
 	}
 
 	@Override
-	public List<Pair<Integer, Integer>> getMovementIndexList(Pair<Integer, Integer> position) {
-		// TODO Auto-generated method stub
-		return null;
+	public List<Pair<Integer, Integer>> getMovementIndexList(Pair<Integer, Integer> index) {
+		List<Pair<Integer, Integer>> list =  new ArrayList<Pair<Integer,Integer>>();
+		Grid targetGrid;
+		if(index.getKey()+1 <=7)
+		{
+			if(index.getValue()-1 >= 0)
+			{
+				targetGrid = ChessWindow.gridList[index.getKey()+1][index.getValue()-1];
+				if(!targetGrid.PieceIsExist())
+					list.add(targetGrid.getIndex());
+			}
+			
+			if(index.getValue()+1 <= 7)
+			{
+				targetGrid = ChessWindow.gridList[index.getKey()+1][index.getValue()+1];
+				if(!targetGrid.PieceIsExist())
+					list.add(targetGrid.getIndex());
+			}
+			
+			targetGrid = ChessWindow.gridList[index.getKey()+1][index.getValue()];
+			if(!targetGrid.PieceIsExist())
+				list.add(targetGrid.getIndex());
+		}
+		
+		if(index.getKey()-1 >=0)
+		{
+			if(index.getValue()-1 >= 0)
+			{
+				targetGrid = ChessWindow.gridList[index.getKey()-1][index.getValue()-1];
+				if(!targetGrid.PieceIsExist())
+					list.add(targetGrid.getIndex());
+			}
+			
+			if(index.getValue()+1 <= 7)
+			{
+				targetGrid = ChessWindow.gridList[index.getKey()-1][index.getValue()+1];
+				if(!targetGrid.PieceIsExist())
+					list.add(targetGrid.getIndex());
+			}
+			
+			targetGrid = ChessWindow.gridList[index.getKey()-1][index.getValue()];
+			if(!targetGrid.PieceIsExist())
+				list.add(targetGrid.getIndex());
+		}
+		
+		if(index.getValue()-1 >= 0)
+		{
+			targetGrid = ChessWindow.gridList[index.getKey()][index.getValue()-1];
+			if(!targetGrid.PieceIsExist())
+				list.add(targetGrid.getIndex());
+		}
+		
+		if(index.getValue()+1 <= 7)
+		{
+			targetGrid = ChessWindow.gridList[index.getKey()][index.getValue()+1];
+			if(!targetGrid.PieceIsExist())
+				list.add(targetGrid.getIndex());
+		}
+		return list;
 	}
 
+	@Override
+	public List<Pair<Integer, Integer>> getAttackIndexList(Pair<Integer, Integer> index) {
+		List<Pair<Integer, Integer>> list =  new ArrayList<Pair<Integer,Integer>>();
+		Grid targetGrid;
+		if(index.getKey()+1 <=7)
+		{
+			if(index.getValue()-1 >= 0)
+			{
+				targetGrid = ChessWindow.gridList[index.getKey()+1][index.getValue()-1];
+				if(targetGrid.PieceIsExist())
+				{
+					if(targetGrid.getPiece().player.getPlayerColour() != player.getPlayerColour())
+						list.add(targetGrid.getIndex());		
+				}
+			}
+			
+			if(index.getValue()+1 <= 7)
+			{
+				targetGrid = ChessWindow.gridList[index.getKey()+1][index.getValue()+1];
+				if(targetGrid.PieceIsExist())
+				{
+					if(targetGrid.getPiece().player.getPlayerColour() != player.getPlayerColour())
+						list.add(targetGrid.getIndex());		
+				}
+			}
+			
+			targetGrid = ChessWindow.gridList[index.getKey()+1][index.getValue()];
+			if(targetGrid.PieceIsExist())
+			{
+				if(targetGrid.getPiece().player.getPlayerColour() != player.getPlayerColour())
+					list.add(targetGrid.getIndex());		
+			}
+		}
+		
+		if(index.getKey()-1 >=0)
+		{
+			if(index.getValue()-1 >= 0)
+			{
+				targetGrid = ChessWindow.gridList[index.getKey()-1][index.getValue()-1];
+				if(targetGrid.PieceIsExist())
+				{
+					if(targetGrid.getPiece().player.getPlayerColour() != player.getPlayerColour())
+						list.add(targetGrid.getIndex());		
+				}
+			}
+			
+			if(index.getValue()+1 <= 7)
+			{
+				targetGrid = ChessWindow.gridList[index.getKey()-1][index.getValue()+1];
+				if(targetGrid.PieceIsExist())
+				{
+					if(targetGrid.getPiece().player.getPlayerColour() != player.getPlayerColour())
+						list.add(targetGrid.getIndex());		
+				}
+			}
+			
+			targetGrid = ChessWindow.gridList[index.getKey()-1][index.getValue()];
+			if(targetGrid.PieceIsExist())
+			{
+				if(targetGrid.getPiece().player.getPlayerColour() != player.getPlayerColour())
+					list.add(targetGrid.getIndex());		
+			}
+		}
+		
+		if(index.getValue()-1 >= 0)
+		{
+			targetGrid = ChessWindow.gridList[index.getKey()][index.getValue()-1];
+			if(targetGrid.PieceIsExist())
+			{
+				if(targetGrid.getPiece().player.getPlayerColour() != player.getPlayerColour())
+					list.add(targetGrid.getIndex());		
+			}
+		}
+		
+		if(index.getValue()+1 <= 7)
+		{
+			targetGrid = ChessWindow.gridList[index.getKey()][index.getValue()+1];
+			if(targetGrid.PieceIsExist())
+			{
+				if(targetGrid.getPiece().player.getPlayerColour() != player.getPlayerColour())
+					list.add(targetGrid.getIndex());		
+			}
+		}
+		return list;
+	}
+	
 	@Override
 	public void AfterMove() {
 		// TODO Auto-generated method stub
 		
 	}
 
-	@Override
-	public List<Pair<Integer, Integer>> getAttackIndexList(Pair<Integer, Integer> index) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+
 }
